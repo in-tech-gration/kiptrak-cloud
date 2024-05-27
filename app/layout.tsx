4e1271d797aede671d8e241f7426accda3854540
Asteris Giannoudis <asteris.gian@gmail.com>
2024-05-27 13:45:46 +0300

    Moved Header and Footer to layout of app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -20,7 +22,9 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
+          <Header />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Welcome from "@/components/Welcome";
-import Footer from "@/components/ui/Footer";
-import Header from "@/components/ui/Header";
 
 export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <Header />
-
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6 justify-center">
           <Welcome />
         </main>
       </div>
-
-      <Footer />
     </div>
   );
 }
