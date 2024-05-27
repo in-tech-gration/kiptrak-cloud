b175883b6a6e298c7e9d7b013b5e5ad6a65f49e2
Asteris Giannoudis <asteris.gian@gmail.com>
2024-05-27 12:59:29 +0300

    Changed metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "KipTrak Cloud",
+  description: "The ideal tool to keep track of your progress!",
 };
 
 export default function RootLayout({
