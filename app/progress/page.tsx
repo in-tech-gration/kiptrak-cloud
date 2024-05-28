dd543c42f456e912f774be9400218bc6fd8ca6a1
Asteris Giannoudis <asteris.gian@gmail.com>
2024-05-28 17:27:51 +0300

    Added Server Side Auth to '/progress' route.

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,6 +1,18 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
 import ProgressGrid from "./ProgressGrid";
 
-export default function ProgressPage() {
+export default async function ProgressPage() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data.user) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
       {/* TODO: Add a course entity to database in order to make this dynamic */}
