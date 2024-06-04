4d1fde344dcf050bd82901f96baafd04518b72e7
Asteris Giannoudis <asteris.gian@gmail.com>
2024-06-04 17:20:47 +0300

    ProgressDashboard client component created.

--- app/progress/[...slug]/page.tsx
+++ app/progress/[...slug]/page.tsx
@@ -1,89 +1,15 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { parseProgressParams } from "@/lib/parsers";
-import { createClient } from "@/utils/supabase/client";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/server";
+import ProgressDashboard from "./ProgressDashboard";
 
-export default function ProgressByWeekAndDay() {
-  const { week, day } = parseProgressParams(
-    useParams<{ slug: string[] }>().slug
-  );
+export default async function ProgressByWeekAndDay() {
   const supabase = createClient();
 
-  const [progressData, setProgressData] = useState<any[] | null>(null);
+  const { data, error } = await supabase.auth.getUser();
 
-  useEffect(() => {
-    const getData = async () => {
-      if (day) {
-        const { data: progress } = await supabase
-          .from("progress_draft")
-          .select()
-          .eq("week", week)
-          .eq("day", day);
-        setProgressData(progress);
-      } else {
-        const { data: progress } = await supabase
-          .from("progress_draft")
-          .select()
-          .eq("week", week);
-        setProgressData(progress);
-      }
-    };
-
-    getData();
-  }, [week, day]);
-
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
-      {!week && !day ? (
-        <h1 className="font-boldd text-4xl text-red-500">Error 404 Here</h1>
-      ) : (
-        <>
-          <h1 className="font-bold text-4xl text-green-500">Progress</h1>
-          {day ? (
-            <h2 className="text-3xl text-gray-400">
-              Week: {week} and Day: {day}
-            </h2>
-          ) : (
-            <h2 className="text-3xl text-gray-400">Week: {week}</h2>
-          )}
-          <div>
-            {progressData?.length ? (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Day</th>
-                    <th>Concept</th>
-                    <th>Task</th>
-                    <th>Level</th>
-                    <th>Instructions</th>
-                    <th>Confidence</th>
-                    <th>Completed</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {progressData?.map((progress, idx) => (
-                    <tr key={`p_${idx}`}>
-                      <th>{progress.day}</th>
-                      <th>{progress.concept}</th>
-                      <th>{progress.task}</th>
-                      <th>{progress.level}</th>
-                      <th>{progress.instructions}</th>
-                      <th>{progress.confidence}</th>
-                      <th>{progress.completed}</th>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            ) : (
-              <h3 className="font-bold text-2xl italic text-red-500">
-                No content found
-              </h3>
-            )}
-          </div>
-        </>
-      )}
-    </div>
-  );
+  if (error || !data.user) {
+    redirect("/login");
+  }
+  return <ProgressDashboard user={data.user} />;
 }
