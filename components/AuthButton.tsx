127188127fc1e32bc4ed278194013c56d2156222
Asteris Giannoudis <asteris.gian@gmail.com>
2024-06-13 12:28:30 +0300

    Link to account page added.

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -19,7 +19,12 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      <Link
+        href="/account"
+        className="font-bold hover:border hover:rounded hover:border-green-500 hover:text-gray-400 p-3"
+      >
+        {user.email}
+      </Link>
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
