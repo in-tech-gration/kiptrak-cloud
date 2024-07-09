e1df590fc8f482b16c783a92a8a4122bd77aed62
Asteris Giannoudis <asteris.gian@gmail.com>
2024-07-09 19:54:02 +0300

    parseProgressParams now looks for a course parameter as well

--- lib/parsers.ts
+++ lib/parsers.ts
@@ -1,12 +1,14 @@
 export const parseProgressParams = (slug: string[]) => {
-  let day, week;
+  let course, day, week;
 
-  if (slug.length === 1) {
-    week = Number(slug[0]);
-  } else if (slug.length === 2) {
-    week = Number(slug[0]);
-    day = Number(slug[1]);
+  if (slug.length === 2) {
+    course = slug[0];
+    week = Number(slug[1]);
+  } else if (slug.length === 3) {
+    course = slug[0];
+    week = Number(slug[1]);
+    day = Number(slug[2]);
   }
 
-  return { day, week };
+  return { course, day, week };
 };
