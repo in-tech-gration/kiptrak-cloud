1393b33668d803210392ad29fe99831cdfd94973
Asteris Giannoudis <asteris.gian@gmail.com>
2024-07-10 18:55:58 +0300

    Changed variable 'course' to 'courseId'

--- lib/parsers.ts
+++ lib/parsers.ts
@@ -1,14 +1,14 @@
 export const parseProgressParams = (slug: string[]) => {
-  let course, day, week;
+  let courseId, day, week;
 
   if (slug.length === 2) {
-    course = slug[0];
+    courseId = slug[0];
     week = Number(slug[1]);
   } else if (slug.length === 3) {
-    course = slug[0];
+    courseId = slug[0];
     week = Number(slug[1]);
     day = Number(slug[2]);
   }
 
-  return { course, day, week };
+  return { courseId, day, week };
 };
