65955842008f3c5eebbc1f5ab47c67230b3dc029
Asteris Giannoudis <asteris.gian@gmail.com>
2024-07-10 17:50:33 +0300

    Used CoursesGrid to make dynamic selection of a course to view progress of.

--- app/progress/ProgressGrid.tsx
+++ app/progress/ProgressGrid.tsx
@@ -1,71 +1,72 @@
-import { Course } from "@/utils/supabase/types";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
-import React from "react";
+import { Course } from "@/utils/supabase/types";
+import CourseGrid from "@/components/CoursesGrid";
 
-type ProgressGridProps = {
-  course: Course;
-};
-
-export default function ProgressGrid(props: ProgressGridProps) {
-  const { course } = props;
+export default function ProgressGrid() {
+  const [course, setCourse] = useState<Course>();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
-      <h1 className="font-bold text-4xl">
-        <span className="text-gray-400">Course</span>:{" "}
-        <span className="text-green-500">{course.name}</span>
-      </h1>
-      <div className="grid 3xl:grid-cols-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
-        {[...Array(course.length)].map((_, i) => (
-          <div
-            key={`week_${i + 1}`}
-            className="grid grid-cols-1 place-items-center bg-white rounded shadow-md outline outline-gray-400"
-          >
-            <p className="font-bold text-green-500">Week {i + 1}</p>
-            <p className="font-bold text-green-500">Days:</p>
-            <div className="p-4 ">
-              <div className="flex ">
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}/1`}
-                >
-                  1
-                </Link>
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}/2`}
-                >
-                  2
-                </Link>
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}/3`}
-                >
-                  3
-                </Link>
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}/4`}
-                >
-                  4
-                </Link>
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}/5`}
-                >
-                  5
-                </Link>
-                <Link
-                  className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
-                  href={`/progress/${course.id}/${i + 1}`}
-                >
-                  *
-                </Link>
+    <>
+      <CourseGrid course={course} setCourse={setCourse} />
+      {course && (
+        <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
+          <h1 className="font-bold text-4xl text-green-500">{course.name}</h1>
+          <div className="grid 3xl:grid-cols-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
+            {[...Array(course.length)].map((_, i) => (
+              <div
+                key={`week_${i + 1}`}
+                className="grid grid-cols-1 place-items-center bg-white rounded shadow-md outline outline-gray-400"
+              >
+                <p className="font-bold text-green-500">Week {i + 1}</p>
+                <p className="font-bold text-green-500">Days:</p>
+                <div className="p-4 ">
+                  <div className="flex ">
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}/1`}
+                    >
+                      1
+                    </Link>
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}/2`}
+                    >
+                      2
+                    </Link>
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}/3`}
+                    >
+                      3
+                    </Link>
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}/4`}
+                    >
+                      4
+                    </Link>
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}/5`}
+                    >
+                      5
+                    </Link>
+                    <Link
+                      className="bg-green-500 text-white py-2 mx-1 rounded text-center h-10 w-10"
+                      href={`/progress/${course.id}/${i + 1}`}
+                    >
+                      *
+                    </Link>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -13,19 +13,9 @@ export default async function ProgressPage() {
     redirect("/login");
   }
 
-  // TODO: Make this dynamic with a useCourseQuery hook
-  const course = {
-    id: "wdx-180",
-    length: 36,
-    description: "Web Development X - 180. Powered by intechgration",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    created_at: "2024-06-07T13:30:35.524847+00:00",
-    name: "WDX-180",
-  };
-
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
-      <ProgressGrid course={course} />
+    <div className="flex-1 w-full flex flex-col gap-5 justify-center items-center">
+      <ProgressGrid />
     </div>
   );
 }
