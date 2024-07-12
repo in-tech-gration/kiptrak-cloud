0398ef254c493146ef6e40dc7e1314246310a4fa
Asteris Giannoudis <asteris.gian@gmail.com>
2024-07-12 18:35:11 +0300

    Initiated static generation [WiP].

--- app/progress/[...slug]/page.tsx
+++ app/progress/[...slug]/page.tsx
@@ -3,6 +3,17 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import ProgressDashboard from "./ProgressDashboard";
 
+// Generate Static URL parameters for static page generation
+export function generateStaticParams() {
+  return [...Array(36)]
+    .map((_, week) =>
+      [...Array(5)]
+        .map((_, day) => [{ slug: ["wdx-180", `${week + 1}`, `${day + 1}`] }])
+        .flat()
+    )
+    .flat();
+}
+
 export default async function ProgressByWeekAndDay() {
   const supabase = createClient();
 
