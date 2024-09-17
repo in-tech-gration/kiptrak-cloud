c3b8e893b2f36f2cbd905283bd453ca2a6bb37ab
Asteris Giannoudis <asteris.gian@gmail.com>
2024-09-17 16:53:35 +0300

    Page '/progress' displays only available courses.

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,10 +1,9 @@
-import ProgressGrid from "./ProgressGrid";
+import CoursesGrid from "@/components/CoursesGrid";
 
 export default function ProgressPage() {
-
   return (
     <div className="flex-1 w-full flex flex-col gap-5 justify-center items-center">
-      <ProgressGrid />
+      <CoursesGrid />
     </div>
   );
 }
