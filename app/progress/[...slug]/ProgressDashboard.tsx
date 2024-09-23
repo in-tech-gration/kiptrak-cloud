12f85f64860dcbea1e91ef7b862eec9c48d43f31
Asteris Giannoudis <asteris.gian@gmail.com>
2024-09-23 17:14:27 +0300

    Cleaner code.

--- app/progress/[...slug]/ProgressDashboard.tsx
+++ app/progress/[...slug]/ProgressDashboard.tsx
@@ -35,46 +35,46 @@ export default function ProgressDashboard() {
     error,
   } = useCourseQuery(courseId as string);
 
-  let content;
-
   if (isLoading) {
-    content = <RotatingLines width="50" />;
-  } else if (isError || !course) {
-    content = (
+    return <RotatingLines width="50" />;
+  }
+
+  if (isError || !course) {
+    return (
       <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
         <h1 className="font-bold text-4xl text-red-500">
           Supabase Error: ${error?.message}
         </h1>
       </div>
     );
-  } else if (course && !week && !day && session?.user.id) {
-    content = <ProgressGrid userId={session.user.id} course={course} />;
-  } else {
-    content = (
-      <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
-        <h1 className="font-bold text-4xl">
-          <span className="text-gray-400">{course.name}'s</span>{" "}
-          <span className="text-green-500">Progress</span>
-        </h1>
-        {day ? (
-          <h2 className="text-3xl text-gray-400">
-            Week: {week} and Day: {day}
-          </h2>
-        ) : (
-          <h2 className="text-3xl text-gray-400">Week: {week}</h2>
-        )}
-        <div style={{ width: "90%" }}>
-          <ProgressSpreadsheet
-            userId={session?.user.id as string}
-            courseId={course.id}
-            week={week}
-            day={day}
-          />
-        </div>
-        {/* TODO: Add previous and next buttons for better user experience */}
-      </div>
-    );
   }
 
-  return content;
+  if (course && !week && !day && session?.user.id) {
+    return <ProgressGrid userId={session.user.id} course={course} />;
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
+      <h1 className="font-bold text-4xl">
+        <span className="text-gray-400">{course.name}'s</span>{" "}
+        <span className="text-green-500">Progress</span>
+      </h1>
+      {day ? (
+        <h2 className="text-3xl text-gray-400">
+          Week: {week} and Day: {day}
+        </h2>
+      ) : (
+        <h2 className="text-3xl text-gray-400">Week: {week}</h2>
+      )}
+      <div style={{ width: "90%" }}>
+        <ProgressSpreadsheet
+          userId={session?.user.id as string}
+          courseId={course.id}
+          week={week}
+          day={day}
+        />
+      </div>
+      {/* TODO: Add previous and next buttons for better user experience */}
+    </div>
+  );
 }
