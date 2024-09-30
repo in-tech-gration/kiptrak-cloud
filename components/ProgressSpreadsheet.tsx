09ae3614640c784ac6dcdfa7fecd7c590ca647b9
Asteris Giannoudis <asteris.gian@gmail.com>
2024-09-30 19:09:55 +0300

    Added message for weeks with no progress.

--- components/ProgressSpreadsheet.tsx
+++ components/ProgressSpreadsheet.tsx
@@ -139,49 +139,60 @@ export const ProgressSpreadsheet = (props: ProgressSpreadsheetProps) => {
     }
   };
 
-  let content;
-
   if (isLoading) {
-    content = <RotatingLines width="50" />;
-  } else if (isError) {
-    content = <div>{error.message}</div>;
-  } else {
-    content = (
-      <>
-        <DataSheetGrid<Progress>
-          lockRows
-          value={progress}
-          onChange={(value) => {
-            setProgress(value);
-          }}
-          columns={
-            admin
-              ? (adminColumnns as Column<Progress>[])
-              : (columns as Column<Progress>[])
-          }
-        />
-        {!updateButtonHidden && (
-          <button
-            className="float-right rounded bg-green-500 p-2 mt-1"
-            onClick={handleSpreadsheetUpdate}
-          >
-            {!mutation.isPending ? (
-              "Update"
-            ) : (
-              <div
-                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                role="status"
-              >
-                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                  Loading...
-                </span>
-              </div>
-            )}
-          </button>
-        )}
-      </>
+    return <RotatingLines width="50" />;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  if (progress.length === 0) {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center bg-gray-900 w-1/3 rounded-3xl p-10">
+          <p className="font-bold text-2xl text-green-500">
+            No Tasks for this week!
+          </p>
+          <p className="font-medium text-xl text-yellow-400">Stay tuned!</p>
+        </div>
+      </div>
     );
   }
 
-  return content;
+  return (
+    <>
+      <DataSheetGrid<Progress>
+        lockRows
+        value={progress}
+        onChange={(value) => {
+          setProgress(value);
+        }}
+        columns={
+          admin
+            ? (adminColumnns as Column<Progress>[])
+            : (columns as Column<Progress>[])
+        }
+      />
+      {!updateButtonHidden && (
+        <button
+          className="float-right rounded bg-green-500 p-2 mt-1"
+          onClick={handleSpreadsheetUpdate}
+        >
+          {!mutation.isPending ? (
+            "Update"
+          ) : (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
+        </button>
+      )}
+    </>
+  );
 };
